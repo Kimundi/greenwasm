2359df0adc2dc877279a4fc253d0befb2707f7c8
Marvin Löbel <loebel.marvin@gmail.com>
2018-06-22 00:58:34 +0200

    wip

--- src/decoding/text_format.rs
+++ src/decoding/text_format.rs
@@ -1,31 +1,33 @@
-enum TokenizerErrorEnum {
+#[derive(Debug)]
+enum ParserErrorEnum {
     UnbalancedBlockComment,
     UnexpectedEof,
     Unrecognized,
     Estr(&'static str),
 }
-use self::TokenizerErrorEnum::*;
-struct TokenizerError {
+use self::ParserErrorEnum::*;
+#[derive(Debug)]
+pub struct ParserError {
     position: usize,
-    error: TokenizerErrorEnum,
+    error: ParserErrorEnum,
 }
-struct Tokenizer<'a> {
+struct Parser<'a> {
     input: &'a str,
     cursor: usize,
 }
 macro_rules! token_parse {
-    ($name:ident($self:ident) -> $t:ty $b:block) => (
-        fn $name(&mut $self) -> Result<$t, TokenizerError> $b
+    ($name:ident($self:ident $(,$arg:ident: $at:ty)*) -> $t:ty $b:block) => (
+        fn $name(&mut $self $(,$arg: $at)*) -> Result<$t, ParserError> $b
     )
 }
-impl<'a> Tokenizer<'a> {
-    fn error_(&self, err: TokenizerErrorEnum) -> TokenizerError {
-        TokenizerError {
+impl<'a> Parser<'a> {
+    fn error_(&self, err: ParserErrorEnum) -> ParserError {
+        ParserError {
             position: self.cursor,
             error: err,
         }
     }
-    fn error(&self, err: TokenizerErrorEnum) -> Result<(), TokenizerError> {
+    fn error(&self, err: ParserErrorEnum) -> Result<(), ParserError> {
         Err(self.error_(err))
     }
     fn unparsed(&self) -> &'a str { &self.input[self.cursor..] }
@@ -53,7 +55,7 @@ enum Token<'a> {
 }
 use self::Token::*;
 
-impl<'a> Tokenizer<'a> {
+impl<'a> Parser<'a> {
     token_parse!(skip_block_comment(self) -> () {
         self.cursor += 2;
         let mut nesting: usize = 1;
@@ -149,19 +151,20 @@ impl<'a> Tokenizer<'a> {
         Ok(&self.input[start..end])
     });
 
+    /*
     token_parse!(parse_token(self) -> Token<'a> {
         Err(())
         .or_else(|_| self.parse_keyword())
-        //.or_else(|_| self.parse_un_sn_fn())
-        //.or_else(|_| self.parse_string())
-        //.or_else(|_| self.parse_id())
-        //.or_else(|_| self.parse_parens())
-        //.or_else(|_| self.parse_reserved())
+        .or_else(|_| self.parse_un_sn_fn())
+        .or_else(|_| self.parse_string())
+        .or_else(|_| self.parse_id())
+        .or_else(|_| self.parse_parens())
+        .or_else(|_| self.parse_reserved())
     });
+    */
 
     token_parse!(parse_keyword(self) -> Token<'a> {
         Ok(match self.skip_peek()? {
-            "???" => unimplemented!(),
             _ => Err(self.error_(Estr("KeywordNotRecognized")))?,
         })
     });
@@ -183,17 +186,144 @@ impl<'a> Tokenizer<'a> {
             _ => Err(self.error_(Estr("NumberNotRecognized")))?,
         })
     });
+    */
+    token_parse!(parse_un(self, bits: u32) -> u64 {
+        let mut s = self.skip_peek()?;
+        let hex = s.starts_with("0x");
+        if hex { s = &s[2..]; }
+
+        let mut n: u64 = 0;
+        let mut allow_underscore = false;
+
+        if !hex {
+            while let Some(c) = s.chars().next() {
+                if c >= '0' && c <= '9' {
+                    n = n.checked_mul(10).ok_or(self.error_(Estr("IntegerOutOfBounds")))?;
+                    n = n.checked_add((c as u64) - ('0' as u64)).ok_or(self.error_(Estr("IntegerOutOfBounds")))?;
+                    allow_underscore = true;
+                } else if allow_underscore && c == '_' {
+                    allow_underscore = false;
+                } else {
+                    self.error(Estr("NotADecDigit"))?;
+                }
+                s = &s[1..];
+            }
+        } else {
+            while let Some(c) = s.chars().next() {
+                if c >= '0' && c <= '9' {
+                    n = n.checked_mul(16).ok_or(self.error_(Estr("IntegerOutOfBounds")))?;
+                    n = n.checked_add((c as u64) - ('0' as u64)).ok_or(self.error_(Estr("IntegerOutOfBounds")))?;
+                    allow_underscore = true;
+                } else if c >= 'a' && c <= 'f' {
+                    n = n.checked_mul(16).ok_or(self.error_(Estr("IntegerOutOfBounds")))?;
+                    n = n.checked_add((c as u64) - ('a' as u64) + 10).ok_or(self.error_(Estr("IntegerOutOfBounds")))?;
+                    allow_underscore = true;
+                } else if c >= 'A' && c <= 'F' {
+                    n = n.checked_mul(16).ok_or(self.error_(Estr("IntegerOutOfBounds")))?;
+                    n = n.checked_add((c as u64) - ('A' as u64) + 10).ok_or(self.error_(Estr("IntegerOutOfBounds")))?;
+                    allow_underscore = true;
+                } else if allow_underscore && c == '_' {
+                    allow_underscore = false;
+                } else {
+                    self.error(Estr("NotAHexDigit"))?;
+                }
+                s = &s[1..];
+            }
+        }
+
+        if (n as u128) >= (1u128 << bits) {
+            self.error(Estr("IntegerOutOfBounds"))?;
+        }
+
+        Ok(n)
+    });
+
+    token_parse!(parse_sn(self, bits: u32) -> i64 {
+        let mut s = self.skip_peek()?;
+        let positive = if s.starts_with('+') {
+            s = &s[1..];
+            true
+        } else if s.starts_with('-') {
+            s = &s[1..];
+            false
+        } else {
+            return Err(self.error_(Estr("IntegerNotRecognized")));
+        };
+
+        let unsigned_n = self.parse_un(64)?;
+        let n: i64;
+
+        if positive {
+            if (unsigned_n as u128) >= (1u128 << (bits - 1)) {
+                self.error(Estr("IntegerOutOfBounds"))?;
+            }
+            n = unsigned_n as i64;
+        } else {
+            if (unsigned_n as u128) > (1u128 << (bits - 1)) {
+                self.error(Estr("IntegerOutOfBounds"))?;
+            }
+            n = -(unsigned_n as i64);
+        }
+
+        Ok(n)
+    });
+    token_parse!(parse_in(self, bits: u32) -> u64 {
+        Err(())
+        .or_else(|_| self.parse_sn(bits).map(|x| x as u64))
+        .or_else(|_| self.parse_un(bits))
+    });
     token_parse!(parse_string(self) -> Token<'a> {
 
+        Err(self.error_(Estr("StringNotRecognized")))
     });
     token_parse!(parse_id(self) -> Token<'a> {
 
+        Err(self.error_(Estr("IdNotRecognized")))
     });
-    */
+    token_parse!(parse_parens(self) -> Token<'a> {
+
+        Err(self.error_(Estr("ParensNotRecognized")))
+    });
+    token_parse!(parse_reserved(self) -> Token<'a> {
+
+        Err(self.error_(Estr("Reserved")))
+    });
 }
 
 use structure::modules::Module;
-pub struct ParserError;
 pub fn from_text_format(_b: &str) -> Result<Module, ParserError> {
     unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn parser(s: &str) -> Parser {
+        Parser {
+            input: s,
+            cursor: 0
+        }
+    }
+
+    fn accept<T>(r: Result<T, ParserError>) {
+        r.unwrap();
+    }
+
+    fn accept_eq<T: PartialEq>(r: Result<T, ParserError>, expected: T) {
+        let is = r.unwrap();
+        assert!(is == expected);
+    }
+
+    fn reject<T>(r: Result<T, ParserError>) {
+        assert!(r.is_err());
+    }
+
+    #[test]
+    fn parse_un() {
+        accept(parser("0").parse_un(8));
+        accept(parser("0").parse_un(16));
+        accept(parser("0").parse_un(32));
+        accept(parser("0").parse_un(64));
+    }
 }
