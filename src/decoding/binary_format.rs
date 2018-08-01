6754a9c26b8f1ea0f598fafd0dcd949ea4c5b923
Marvin Löbel <loebel.marvin@gmail.com>
2018-08-01 14:17:39 +0200

    binary parse sN

--- src/decoding/binary_format.rs
+++ src/decoding/binary_format.rs
@@ -16,17 +16,67 @@ fn parse_vec<'a, F, B>(input: Inp<'a>, mut parse_b: F) -> IResult<Inp<'a>, Vec<B
 named!(parse_byte <Inp, u8>, map!(take!(1), |n| n[0]));
 
 named_args!(parse_uN(N: u32) <Inp, u64>, alt!(
-    verify!(map!(parse_byte, |n| n as u64), |n| (n < (1 << 7)) && ((n as u128) < (1 << N)))
+    do_parse!(
+        n: verify!(parse_byte, |n| {
+            // n < 2^7 ∧ n < 2^N
+            let n = n as u128;
+            let v27 = 1 << 7;
+            let v2N = 1 << N;
+            n < v27 && n < v2N
+        })
+        >> (n as u64)
+    )
     | do_parse!(
-        n: verify!(map!(parse_byte, |n| n as u64), |n| (n >= (1 << 7)) && (N > 7))
+        n: verify!(parse_byte, |n| {
+            // n ≥ 2^7 ∧ N > 7
+            let n = n as u128;
+            let v27 = 1 << 7;
+            n >= v27 && N > 7
+        })
         >> m: apply!(parse_uN, N - 7)
-        >> ((1 << 7) * m + (n - (1 << 7)))
+        >> ((1 << 7) * m + ((n as u64) - (1 << 7)))
     )
 ));
 
 named!(pub parse_u32 <Inp, u32>, map!(apply!(parse_uN, 32), |x| x as u32));
 named!(pub parse_u64 <Inp, u64>, apply!(parse_uN, 64));
 
+named_args!(parse_sN(N: u32) <Inp, i64>, alt!(
+    do_parse!(
+        n: verify!(parse_byte, |n| {
+            // n < 2^6 ∧ n < 2^(N−1)
+            let n = n as i128;
+            let v26 = 1 << 6;
+            let v2N1 = 1 << (N - 1);
+            n < v26 && n < v2N1
+        })
+        >> (n as i64)
+    )
+    | do_parse!(
+        n: verify!(parse_byte, |n| {
+            // 2^6 ≤ n < 2^7 ∧ n ≥ 2^7 − 2^(N − 1)
+            let n = n as i128;
+            let v26 = 1 << 6;
+            let v27 = 1 << 7;
+            let v2N1 = 1 << (N - 1);
+            v26 <= n && n < v27 && n >= (v27 - v2N1)
+        })
+        >> (n as i64 - (1 << 7))
+    )
+    | do_parse!(
+        n: verify!(parse_byte, |n| {
+            // n ≥ 2^7 ∧ N > 7
+            let n = n as i128;
+            let v27 = 1 << 7;
+            n >= v27 && N > 7
+        })
+        >> m: apply!(parse_sN, N - 7)
+        >> ((1 << 7) * m + ((n as i64) - (1 << 7)))
+    )
+));
+
+named!(pub parse_s32 <Inp, i32>, map!(apply!(parse_sN, 32), |x| x as i32));
+named!(pub parse_s64 <Inp, i64>, apply!(parse_sN, 64));
 
 
 #[cfg(test)]
--- src/decoding/tests_binary_format.rs
+++ src/decoding/tests_binary_format.rs
@@ -11,38 +11,38 @@ fn check<'a, T, F>(mut parse: F, input: &'a [u8], res: CheckRes<T>)
     where T: Debug + PartialEq,
           F: Fn(Inp<'a>) -> IResult<Inp<'a>, T>,
 {
+    let mut err = None;
     match parse(CompleteByteSlice(input)) {
         Ok((CompleteByteSlice(&[]), is)) => {
             if let OkWith(should) = res {
-                assert_eq!(is, should);
+                if is != should {
+                    err = Some(format!("Should have parsed with {:?}, but is {:?}", should, is));
+                }
             } else {
-                panic!("Should have failed, but parsed with {:?}", is);
+                err = Some(format!("Should have failed, but parsed with {:?}", is));
             }
         }
         Ok((CompleteByteSlice(x), is)) => {
-            panic!("Accepted without parsing all input. Remaining: {:?}, result: {:?}", x, is);
+            err = Some(format!("Accepted without parsing all input. Remaining: {:?}, result: {:?}", x, is));
         }
         Err(x) => {
             if let OkWith(should) = res {
-                panic!("Should have parsed with {:?}, but failed with {:?}", should, x);
+                err = Some(format!("Should have parsed with {:?}, but failed with {:?}", should, x));
             }
         }
     }
-}
-
-#[test]
-fn test_parse_u32() {
-    test_parse_uN(|inp| parse_u32(inp).map(|(i, x)| (i, x as u64)), 32);
-}
-
-#[test]
-fn test_parse_u64() {
-    test_parse_uN(parse_u64, 64);
+    if let Some(err) = err {
+        panic!("\n\nERROR: {}\nInput:\n{:?}\n\n", err, input);
+    } else {
+        // println!("check OK");
+    }
 }
 
 fn test_parse_uN<F>(parse: F, bits: u32)
     where F: Fn(Inp) -> IResult<Inp, u64>,
 {
+    check(&parse, &[0x00], OkWith(0x00));
+
     check(&parse, &[0x00], OkWith(0x00));
     check(&parse, &[0x7f], OkWith(0x7f));
     check(&parse, &[0xff], Failed);
@@ -63,4 +63,59 @@ fn test_parse_uN<F>(parse: F, bits: u32)
     check(&parse, &[0xff, 0xff, 0xff, 0x7f], OkWith(0b1111111_1111111_1111111_1111111));
     check(&parse, &[0b1_0011111, 0b1_0001111, 0b1_0000111, 0b0_0000011],
              OkWith(0b__0000011______0000111______0001111______0011111));
+}
+
+#[test]
+fn test_parse_u32() {
+    test_parse_uN(|inp| parse_u32(inp).map(|(i, x)| (i, x as u64)), 32);
+}
+
+#[test]
+fn test_parse_u64() {
+    test_parse_uN(parse_u64, 64);
+}
+
+fn test_parse_sN<F>(parse: F, bits: u32)
+    where F: Fn(Inp) -> IResult<Inp, i64>,
+{
+    check(&parse, &[0x00], OkWith(0x00));
+
+    check(&parse, &[0x00], OkWith(0x00));
+    check(&parse, &[0x3f], OkWith(0x3f));
+    check(&parse, &[0x7f], OkWith(-0x01));
+    check(&parse, &[0xff], Failed);
+
+    check(&parse, &[0xff, 0x00], OkWith(0x7f));
+    check(&parse, &[0xff, 0x01], OkWith(0xff));
+    check(&parse, &[0xff, 0x80, 0x00], OkWith(0x7f));
+    check(&parse, &[0xff, 0x80, 0x80, 0x00], OkWith(0x7f));
+    check(&parse, &[0xff, 0x80, 0x80, 0x80, 0x00], OkWith(0x7f));
+
+    check(&parse, &[0x7f], OkWith(-1));
+    check(&parse, &[0xff, 0x7f], OkWith(-1));
+    check(&parse, &[0xff, 0xff, 0x7f], OkWith(-1));
+    check(&parse, &[0xff, 0xff, 0xff, 0x7f], OkWith(-1));
+    check(&parse, &[0xff, 0xff, 0xff, 0xff, 0x7f], OkWith(-1));
+
+    if bits == 32 {
+        check(&parse, &[0xff, 0x80, 0x80, 0x80, 0x80, 0x00], Failed);
+    } else {
+        check(&parse, &[0xff, 0x80, 0x80, 0x80, 0x80, 0x00], OkWith(0x7f));
+        check(&parse, &[0xff, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x00], OkWith(0x7f));
+        check(&parse, &[0xff, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x00], Failed);
+    }
+
+    check(&parse, &[0xff, 0xff, 0xff, 0x7f], OkWith(-1));
+    check(&parse, &[0b1_0011111, 0b1_0001111, 0b1_0000111, 0b0_0000011],
+             OkWith(0b__0000011______0000111______0001111______0011111));
+}
+
+#[test]
+fn test_parse_s32() {
+    test_parse_sN(|inp| parse_s32(inp).map(|(i, x)| (i, x as i64)), 32);
+}
+
+#[test]
+fn test_parse_s64() {
+    test_parse_sN(parse_s64, 64);
 }
