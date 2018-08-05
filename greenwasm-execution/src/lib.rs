9e4d9a82023e6fd026952123557e8461783076c7
Marvin Löbel <loebel.marvin@gmail.com>
2018-08-05 16:43:13 +0200

    Implement runtime structure

--- greenwasm-execution/src/lib.rs
+++ greenwasm-execution/src/lib.rs
@@ -1,1 +1,3 @@
 extern crate greenwasm_structure as structure;
+
+pub mod runtime_structure;
---
+++ greenwasm-execution/src/runtime_structure.rs
@@ -0,0 +1,103 @@
+use structure::types::*;
+use structure::modules::*;
+use structure::instructions::*;
+
+pub enum Val {
+    I32(I32),
+    I64(I64),
+    F32(F32),
+    F64(F64),
+}
+
+pub enum Result {
+    Vals(Vec<Val>),
+    Trap,
+}
+
+pub struct Store {
+    pub funcs: Vec<FuncInst>,
+    pub tables: Vec<TableInst>,
+    pub mems: Vec<MemInst>,
+    pub globals: Vec<GlobalInst>,
+}
+
+pub struct FuncAddr(pub usize);
+
+pub struct TableAddr(pub usize);
+
+pub struct MemAddr(pub usize);
+
+pub struct GlobalAddr(pub usize);
+
+pub struct ModuleInst {
+    pub types: Vec<FuncType>,
+    pub funcaddrs: Vec<FuncAddr>,
+    pub tableaddrs: Vec<TableAddr>,
+    pub memaddrs: Vec<MemAddr>,
+    pub globaladdrs: Vec<GlobalAddr>,
+    pub exports: Vec<ExportInst>,
+}
+
+pub enum FuncInst {
+    Internal {
+        type_: FuncType,
+        module: ModuleInst,
+        code: Func
+    },
+    Host {
+        type_: FuncType,
+        hostcode: HostFunc
+    },
+}
+
+pub struct HostFunc {
+    pub id: usize
+}
+
+pub struct TableInst {
+    pub elem: Wec<FuncElem>,
+    pub max: Option<u32>
+}
+
+pub struct FuncElem(pub Option<FuncAddr>);
+
+pub struct MemInst {
+    pub data: Wec<Byte>,
+    pub max: Option<u32>,
+}
+
+pub struct GlobalInst {
+    pub value: Val,
+    pub mutability: Mut,
+}
+
+pub struct ExportInst {
+    pub name: Name,
+    pub value: ExternVal,
+}
+
+pub enum ExternVal {
+    Func(FuncAddr),
+    Table(TableAddr),
+    Mem(MemAddr),
+    Global(GlobalAddr),
+}
+
+pub type Stack = Vec<StackElem>;
+
+pub enum StackElem {
+    Val(Val),
+    Label {
+        n: usize, // NB. Can only be 0 or 1
+        branch_target: Vec<Instr>, // TODO: change implementation to something more sane
+    },
+    Activation {
+        n: usize, // NB. Can only be 0 or 1
+        frame: Frame,
+    },
+}
+
+pub struct Frame {
+    pub locals: Vec<Val>,
+    pub module: ModuleInst, // TODO: change implementation to something more sane
+}
